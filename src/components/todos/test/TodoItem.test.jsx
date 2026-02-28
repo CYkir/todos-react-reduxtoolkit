159ba7076c7206a0f441f3457ef4389b1f742e55
CYkir <mvzakiir@gmail.com>
2026-02-28 21:40:27 +0700

    test todoform

---
+++ src/components/todos/test/TodoItem.test.jsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "../../store/slices/todoSlice";
+import TodoItem from "./TodoItem";
+import {  test, expect } from "vitest";
+
+const store = configureStore({
+  reducer: { todos: todoReducer },
+});
+
+test("match snapshot", () => {
+  const { asFragment } = render(
+    <Provider store={store}>
+      <TodoItem todo={{ id: 1, title: "Test Todo", completed: false }} />
+    </Provider>,
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
