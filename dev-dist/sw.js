c2d50a890db412cd17f6c25b8e24e08539169e5e
CYkir <mvzakiir@gmail.com>
2026-02-28 23:48:43 +0700

    link vidio penjelasan

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ags5e197ql"
+    "revision": "0.k79uv8dfngk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/todos/test/TodoItem.test.jsx
+++ src/components/todos/test/TodoItem.test.jsx
@@ -2,8 +2,14 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "@/store/slices/todoSlice";
+import { test, expect, vi } from "vitest";
+
+
+vi.mock("../../common/ConfirmModal", () => ({
+  default: () => <div data-testid="mock-confirm-modal" />,
+}));
+
 import TodoItem from "../TodoItem";
-import {  test, expect } from "vitest";
 
 const store = configureStore({
   reducer: { todos: todoReducer },
